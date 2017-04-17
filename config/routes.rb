62f2c4cb5868656bdf5e6886250e6bae4b0aa0cf
jiangxinli <15009218266@163.com>
2017-04-17 21:51:12 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
     resources :groups do
+      member do
+        post :join
+      end
       resources :posts
     end
     root 'groups#index'
